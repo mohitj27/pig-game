e931ef53b179957afe7b653d3bb30c3106716207
mohitj27 <mjain8156@gmail.com>
2017-11-15 11:47:38 +0530

    upload project files

---
+++ motivate.js
@@ -0,0 +1,22 @@
+document.querySelector('#make new id of Men').addEventListener('click', function() {
+    document.querySelector('#make new id of women').classList.remove('dropdown menu-mobile-open ');
+    document.querySelector('#make new id of Lifestyle').classList.remove('dropdown menu-mobile-open ');
+    document.querySelector('#make new id of WallArts').classList.remove('dropdown menu-mobile-open ');
+}
+
+document.querySelector('#use new id of women').addEventListener('click', function() {
+    document.querySelector('# id of men').classList.remove('dropdown menu-mobile-open ');
+    document.querySelector('id of Lifestyle').classList.remove('dropdown menu-mobile-open ');
+    document.querySelector('# id of WallArts').classList.remove('dropdown menu-mobile-open ');
+}
+document.querySelector('#id of Lifestyle').addEventListener('click', function() {
+    document.querySelector('#id of women').classList.remove('dropdown menu-mobile-open ');
+    document.querySelector('#id of men').classList.remove('dropdown menu-mobile-open ');
+    document.querySelector('#id of WallArts').classList.remove('dropdown menu-mobile-open ');
+}
+
+document.querySelector('#id of WallArts').addEventListener('click', function() {
+    document.querySelector('#id of women').classList.remove('dropdown menu-mobile-open ');
+    document.querySelector('#id of men').classList.remove('dropdown menu-mobile-open ');
+    document.querySelector('#id of Lifestyle').classList.remove('dropdown menu-mobile-open ');
+}
